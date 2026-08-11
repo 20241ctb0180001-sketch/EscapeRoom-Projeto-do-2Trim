47a283cd1fc3ec627f801438d4b4f2f7adc4341f
ViinPerez-sketch-0180001 <20241ctb0180001@estudantes.ifpr.edu.br>
2026-08-11 14:12:08 -0300

    a

--- SalaDeEscape_2trim/Assets/Scripts/PlayerInteract.cs
+++ SalaDeEscape_2trim/Assets/Scripts/PlayerInteract.cs
@@ -145,8 +145,6 @@ public class PlayerInteract : MonoBehaviour
         while (timer > 1f)
         {
             obj.transform.position = Vector3.Lerp(OriginPos, pos, timer);
-            //*pos = Mycam.transform.position + (transform.rotation * Vector3.forward * RayDistance);
-            obj.transform.position = Vector3.Lerp(OriginPos, pos, timer);
             obj.transform.rotation = Quaternion.Lerp(OiginRotat, objViewer.rotation, timer);
             timer += Time.deltaTime * 8f;
             yield return null;
