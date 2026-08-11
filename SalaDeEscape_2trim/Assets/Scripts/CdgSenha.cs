4cd18022487aafcde28f6ac0595e1415c032a3ff
0180001-sketch-ViinPerez <20233024265@estudantes.ifpr.edu.br>
2026-08-11 15:33:05 -0300

    codiguin da senha

---
+++ SalaDeEscape_2trim/Assets/Scripts/CdgSenha.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.InputSystem;
+
+public class CdgSenha : MonoBehaviour
+{
+
+    private Animator anim;
+
+    public InputActionAsset inputAction;
+    private InputAction topacoEs;
+
+    private bool emAlcance = false;
+
+    [SerializeField] private TextMeshProUGUI cdgText;
+    string cdgValor = "";
+    public string senha;
+    public GameObject painelCdg;
+
+    void Awake()
+    {
+        topacoEs = InputSystem.actions.FindAction("InteractE");
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        cdgText.text = cdgValor;
+
+        if(cdgValor == senha)
+        {
+            anim.SetTrigger("abrirTampa");
+            painelCdg.SetActive(false);
+        }
+
+        if(cdgValor.Length >= 7)
+        {
+            cdgValor = "";
+        }
+
+        if(topacoEs.WasPressedThisFrame() && emAlcance == true)
+        {
+            painelCdg.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            emAlcance = true;
+        }
+
+        if(painelCdg.activeInHierarchy)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        emAlcance = false;
+        painelCdg.SetActive(false);
+    }
+
+    public void AddDigit(string digito)
+    {
+        cdgValor += digito;
+    }
+}
