3807bf3b17be4450c831ff835cde24af056e8a19
BiancaJ25 <20233024265@estudantes.ifpr.edu.br>
2026-08-16 16:45:26 -0300

    caceta de animacao funfa

--- SalaDeEscape_2trim/Assets/Scripts/CdgSenha.cs
+++ SalaDeEscape_2trim/Assets/Scripts/CdgSenha.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class CdgSenha : MonoBehaviour
 {
 
-    //private Animator anim;
+    private Animator anim;
 
     public InputActionAsset inputAction;
     private InputAction topacoEs;
@@ -25,7 +25,7 @@ public class CdgSenha : MonoBehaviour
 
     void Start()
     {
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -34,7 +34,7 @@ public class CdgSenha : MonoBehaviour
 
         if(cdgValor == senha)
         {
-            //anim.SetTrigger("abrirTampa");
+            anim.SetTrigger("abrirTampa");
             painelCdg.SetActive(false);
         }
 
