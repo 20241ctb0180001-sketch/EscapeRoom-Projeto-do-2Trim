f9a30ff7416b4931d70345b7e0da887f38b83e71
matheussaur-v1 <20241ctb0180016@estudantes.ifpr.edu.br>
2026-08-18 14:59:49 -0300

    vou chorar

---
+++ SalaDeEscape_2trim/Assets/Scripts/inventario dos brinquedos/BrinquedoColetavel.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class BrinquedoColetavel : MonoBehaviour
+{
+    [Header("Configurações do Item")]
+    public Item dadosDoItem; // O ScriptableObject com nome e ícone
+}
