cffd47cbbf04850cc425d43d101ce22aff7b8b95
ViinPerez-sketch-0180001 <20241ctb0180001@estudantes.ifpr.edu.br>
2026-09-01 15:57:36 -0300

    respawn concertado

--- SalaDeEscape_2trim/Assets/Scripts/Puzzles/DefinirSpawn.cs
+++ SalaDeEscape_2trim/Assets/Scripts/Puzzles/DefinirSpawn.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class DefinirSpawn : MonoBehaviour
 {
-    private int nuRespawn;
+    [SerializeField]private int nuRespawn;
     public GameObject Player;
     public GameObject objetoAlvo;
     private PlayerLimit alvoScript;
@@ -11,25 +11,20 @@ public class DefinirSpawn : MonoBehaviour
         if (objetoAlvo != null)
         {
             alvoScript = objetoAlvo.GetComponent<PlayerLimit>();
-            
-        }
-        if (gameObject.name == "Respawn1")
-        {
-            nuRespawn = 1;
-        }else if (gameObject.name == "Respawn2")
-        {
-            nuRespawn = 2;
         }
     }
-
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == Player)
+        if (gameObject.tag == "respawn1")
         {
-            print("Respawn Ativado");
-            alvoScript.QualSpawnEsta(nuRespawn);
+            alvoScript.currRespawn1 = true;
+            Debug.Log("Respawn1 ativado");
+        }
+        else if (gameObject.tag == "respawn2")
+        {
+            alvoScript.currRespawn2 = true;
+            Debug.Log("Respawn2 ativado");
         }
     }
-    
 }
